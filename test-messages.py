951cbf35a58ea2757d249276babb683f402fe493
Fabian Affolter <mail@fabian-affolter.ch>
2013-08-29 11:47:22 +0200

    Typo fixed, new areas added

--- test-messages.py
+++ test-messages.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # 
-# test-messages.py - This script pushlish a random MQTT messages every 2 s.
+# test-messages.py - This script publish a random MQTT messages every 2 s.
 #
 # Copyright (c) 2013, Fabian Affolter <fabian@affolter-engineering.ch>
 # Released under the MIT license. See LICENSE file for details.
@@ -13,6 +13,7 @@ broker = '127.0.0.1'
 port = 1883
 element = 'home'
 areas = ['front', 'back', 'kitchen', 'basement', 'living']
+entrances = ['door', 'window']
 states = ['true', 'false']
 
 print 'Messages are published on topic %s/#... -> CTRL + C to shutdown' \
@@ -24,7 +25,7 @@ while True:
         topic = element + '/' + area + '/temp'
         message = random.randrange(0, 30, 1)
     else:
-        topic = element + '/' + area + '/door'
+        topic = element + '/' + area + '/' + random.choice(entrances)
         message = random.choice(states)
 
     client = mosquitto.Mosquitto("mqtt-panel-test")
