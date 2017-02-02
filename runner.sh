2dd3e74aa7895192de697ce86e5c47b5061c1bb4
Fabian Affolter <mail@fabian-affolter.ch>
2017-02-02 11:36:53 +0100

    Update year

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # 
-# Copyright (c) 2013-2016, Fabian Affolter <fabian@affolter-engineering.ch>
+# Copyright (c) 2013-2017, Fabian Affolter <fabian@affolter-engineering.ch>
 # Released under the MIT license. See LICENSE file for details.
 #
 node server.js
