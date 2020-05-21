3cc277fea87b02a92a0d95ad598bf4a8e550562b
Fabian Affolter <mail@fabian-affolter.ch>
2020-05-21 09:55:36 +0200

    FOrmatting with black

--- test-messages.py
+++ test-messages.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 
+#
 # test-messages.py - This script publish a random MQTT messages every 2 s.
 #
 # Copyright (c) 2013-2020, Fabian Affolter <fabian@affolter-engineering.ch>
@@ -11,27 +11,25 @@ import paho.mqtt.client as mqtt
 
 timestamp = int(time.time())
 
-broker = '127.0.0.1'
+broker = "127.0.0.1"
 port = 1883
-element = 'home'
-areas = ['front', 'back', 'kitchen', 'basement', 'living']
-entrances = ['door', 'window']
-states = ['true', 'false']
+element = "home"
+areas = ["front", "back", "kitchen", "basement", "living"]
+entrances = ["door", "window"]
+states = ["true", "false"]
 
-print('Messages are published on topic %s/#... -> CTRL + C to shutdown' \
-    % element)
+print(f"Messages are published on topic {element}/#... -> CTRL + C to shutdown")
 
 while True:
     area = random.choice(areas)
-    if (area in ['basement', 'living']):
-        topic = element + '/' + area + '/temp'
+    if area in ["basement", "living"]:
+        topic = element + "/" + area + "/temp"
         message = random.randrange(0, 30, 1)
     else:
-        topic = element + '/' + area + '/' + random.choice(entrances)
+        topic = element + "/" + area + "/" + random.choice(entrances)
         message = random.choice(states)
 
-    mqttclient = mqtt.Client("mqtt-panel-test", protocol=mqtt.MQTTv311)
-    mqttclient.connect(broker, port=int(port))
-    mqttclient.publish(topic, message)
+    mqtt_client = mqtt.Client("mqtt-panel-test", protocol=mqtt.MQTTv311)
+    mqtt_client.connect(broker, port=int(port))
+    mqtt_client.publish(topic, message)
     time.sleep(2)
-
